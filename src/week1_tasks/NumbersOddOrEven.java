32858504bd5d068a2db3ad12209d8e118869a52b
Hekuran <hekuranhalitaj@hotmail.com>
2023-05-21 15:55:48 +0200

    added changes

--- src/week1_tasks/NumbersOddOrEven.java
+++ src/week1_tasks/NumbersOddOrEven.java
@@ -1,4 +1,27 @@
 package week1_tasks;
 
 public class NumbersOddOrEven {
-}
+
+    public static void main(String[] args) {
+
+        int nr1 = 5;
+        if(nr1 % 2 == 0){
+            System.out.println("Even");
+        }else{
+            System.out.println("Odd");
+        }
+        int nr2 = 6;
+        if(nr2 % 2 == 0){
+            System.out.println("Even");
+        }else{
+            System.out.println("Odd");
+        }
+
+
+    }
+
+
+    }
+
+
+
