179db24d9c33b9c26dfae7ee849784b161a40df5
Hekuran <hekuranhalitaj@hotmail.com>
2023-05-22 09:57:50 +0200

    added changes

---
+++ src/week1_tasks/Divisionwithoutdivope.java
@@ -0,0 +1,20 @@
+package week1_tasks;
+import java.util.Scanner;
+public class Divisionwithoutdivope {
+    public static void main(String args[]){//main method
+        int num1,num2,div,result=0;
+        Scanner scan=new Scanner(System.in);
+
+        System.out.print("Enter the value to num1: ");
+        num1=scan.nextInt();
+        System.out.print("Enter the value to num2: ");
+        num2=scan.nextInt();
+        div=num1+num2;
+        while(div>num2){
+            div=div-num2;
+            result++;
+
+        }
+        System.out.println("Division of "+num1+" and "+num2+":"+result);
+    }
+}
